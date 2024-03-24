2e046e5c446db6717fc659e5adb19ee3f03aa84e
SheharaJ <sheharajaya7@gmail.com>
2024-03-24 19:29:23 +0530

    Initial Commit

---
+++ SingheClothing/SingheClothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingheClothingApp.swift
+//  SingheClothing
+//
+//  Created by Shehara Jayasooriya on 2024-03-24.
+//
+
+import SwiftUI
+
+@main
+struct SingheClothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
